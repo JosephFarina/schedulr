b1be07e580849ca318c26494fa5c968317bdf62b
Joey Farina <jrf61194@gmail.com>
2017-02-19 22:29:13 -0500

    table cell done

--- src/shared/components/EntityTable/EntityTableCell.tsx
+++ src/shared/components/EntityTable/EntityTableCell.tsx
@@ -7,9 +7,97 @@ const Icon = require('antd/lib/icon')
 const Input = require('antd/lib/input')
 const Select = require('antd/lib/select')
 
+/**
+ * 
+ * Table Cell Helper Components
+ * 
+ */
+
+interface InputCellProps {
+  value: string
+  handleChange: (val: any) => void
+  handleSubmit: (val: any) => void
+  handleExit: (val: any) => void
+  hasBeenEdited: boolean
+  selectOptions: Entity[]
+  children?: any
+}
+
+const EditIcons = ({handleSubmit, handleExit, hasBeenEdited}: InputCellProps) => (
+  <span>
+    {hasBeenEdited && <Icon
+      type="check"
+      className={styles.iconCheck}
+      onClick={handleSubmit}
+    />}
+    <Icon type="close" className={styles.iconClose}
+      onClick={handleExit} />
+  </span>
+)
+
+const InputWrapper = (props: InputCellProps) => (
+  <div className={styles.inputWrapper}>
+    {props.children}
+    <EditIcons {...props} />
+  </div>
+)
+
+const InputCell = (props: InputCellProps) => {
+  const {value, handleChange, handleSubmit} = props
+  return (
+    <InputWrapper {...props}>
+      <Input
+        value={value}
+        onChange={handleChange}
+        onPressEnter={handleSubmit}
+      />
+    </InputWrapper>
+  )
+}
+
+const SelectCell = (props: InputCellProps) => {
+  const {value, handleChange, selectOptions} = props
+  return (
+    <InputWrapper {...props}>
+      <Select
+        style={{ width: '100%' }}
+        value={value}
+        onChange={handleChange}
+        children={selectOptions.map(ent => <Select.Option value={ent.id}>{ent.alias}</Select.Option>)}
+      />
+    </InputWrapper>
+  )
+}
+
+
+interface TextCellProps {
+  value: string
+  handleEditRequest: (val: any) => any
+}
+
+const TextCell = (props: TextCellProps) => (
+  <div className={styles.textWrapper}>
+    {props.value || ' '}
+    <Icon
+      type="edit"
+      className={styles.icon}
+      onClick={props.handleEditRequest}
+    />
+  </div>
+)
+
+const InputType = (props: InputCellProps) => props.selectOptions ? <SelectCell {...props} /> : <InputCell {...props} />
+
+
+/**
+ * 
+ * Table Cell
+ * 
+ */
+
+
 interface Props {
   value?: string
-
   onChange?: (val: string) => any
   selectOptions?: Entity[]
 }
@@ -20,60 +108,10 @@ interface State {
   hasBeenEdited?: boolean
 }
 
-
-const InputCell = ({value, handleChange, handleSubmit, handleExit}) => (
-  <div className={styles.inputWrapper}>
-    <Input
-      value={value}
-      onChange={handleChange}
-      onPressEnter={handleSubmit}
-    />
-    <Icon
-      type="check"
-      className={styles.iconCheck}
-      onClick={handleSubmit}
-    />
-    <Icon type="close" className={styles.iconClose}
-      onClick={handleExit} />
-  </div>
-)
-
-const SelectCell = ({value, handleChange, handleSubmit, handleExit, entities, hasBeenEdited}) => (
-  <div className={styles.inputWrapper}>
-    <Select
-      style={{ width: '100%' }}
-      value={value}
-      onChange={handleChange}
-    >
-      {entities.map(ent => <Select.Option value={ent.id}>{ent.alias}</Select.Option>)}
-
-    </Select>
-    {hasBeenEdited && <Icon
-      type="check"
-      className={styles.iconCheck}
-      onClick={handleSubmit}
-    />}
-    <Icon type="close" className={styles.iconClose}
-      onClick={handleExit} />
-  </div>
-)
-
-const TextCell = ({value, handleEditRequest}) => (
-  <div className={styles.textWrapper}>
-    {value || ' '}
-    <Icon
-      type="edit"
-      className={styles.icon}
-      onClick={handleEditRequest}
-    />
-  </div>
-)
-
-
-
 export class EntityTableCell extends React.Component<Props, State> {
   static defaultProps: Props = {
-    selectOptions: null
+    selectOptions: null,
+    value: ''
   }
 
   constructor(props) {
@@ -90,46 +128,36 @@ export class EntityTableCell extends React.Component<Props, State> {
     this.setState({ value, hasBeenEdited: true })
   }
 
+  resetToText() {
+    this.setState({ editable: false, value: '' })
+  }
+
   handleSubmit = e => {
-    this.setState({ editable: false })
     this.props.onChange(this.state.value)
+    this.resetToText()
   }
 
-  handleExit = e => {
-    this.setState({ editable: false })
-  }
-
-  edit = e => {
+  handleEditRequest = e => {
     this.setState({ editable: true, value: this.props.value, hasBeenEdited: false })
   }
 
   render() {
-    const {editable, value: stateValue } = this.state
-    const {value: propsValue, selectOptions } = this.props
     return (
       <div className={styles.editableCell}>
         {
-          editable ?
-            selectOptions ?
-              <SelectCell
-                {...this.state}
-                handleChange={this.handleChange}
-                value={stateValue}
-                entities={selectOptions}
-                handleSubmit={this.handleSubmit}
-                handleExit={this.handleExit}
-              />
-              :
-              <InputCell
-                handleChange={this.handleChange}
-                value={stateValue}
-                handleSubmit={this.handleSubmit}
-                handleExit={this.handleExit}
-              />
+          this.state.editable ?
+            <InputType
+              value={this.state.value}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              handleExit={this.resetToText}
+              hasBeenEdited={this.state.hasBeenEdited}
+              selectOptions={this.props.selectOptions}
+            />
             :
             <TextCell
-              handleEditRequest={this.edit}
-              value={propsValue}
+              handleEditRequest={this.handleEditRequest}
+              value={this.props.value}
             />
         }
       </div>
